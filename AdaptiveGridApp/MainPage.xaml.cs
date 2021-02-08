fad7adbd2fdc78820ccc6aee67e5cff5f2b398b1
prithvi.v <prithvi.v@zohocorp.com>
2021-02-08 14:59:48 +0530

    Margin computation added

--- AdaptiveGridApp/MainPage.xaml.cs
+++ AdaptiveGridApp/MainPage.xaml.cs
@@ -28,7 +28,8 @@ namespace AdaptiveGridApp
         ObservableCollection<PhotoItem> PhotoItems = new ObservableCollection<PhotoItem>();
         IList<PhotoItem> PhotoItemsList = new List<PhotoItem>();
         private int CurrentIndex = 0;
-        public int Columns = 1;
+        public int TotalColumns = 1;
+        public int TotalRows = 1;
         public int MinimumWidth = 350;
         public MainPage()
         {
@@ -41,14 +42,19 @@ namespace AdaptiveGridApp
 
         private void ComputeAndSetDimension()
         {
-            Columns = (int)Math.Ceiling(Math.Sqrt(PhotoItems.Count));
-            double itemWidth = AdaptiveGridViewControl.ActualWidth / Columns;
+            int cols = (int)Math.Ceiling(Math.Sqrt(PhotoItems.Count));
+            if (cols > 0)
+                TotalColumns = cols;
+            double itemWidth = AdaptiveGridViewControl.ActualWidth / TotalColumns;
             if (itemWidth < MinimumWidth)
             {
                 int MaxColumns = (int)(AdaptiveGridViewControl.ActualWidth / MinimumWidth);
-                Columns = MaxColumns;
+                TotalColumns = MaxColumns;
             }
-            AdaptiveGridViewControl.DesiredWidth = AdaptiveGridViewControl.ActualWidth / Columns;
+            if (TotalColumns > 0)
+                TotalRows = (int)Math.Ceiling(PhotoItems.Count / (double)TotalColumns);
+            AdaptiveGridViewControl.DesiredWidth = AdaptiveGridViewControl.ActualWidth / TotalColumns;
+            // ComputeAndSetMargins();
         }
         private void AddParticipant_Click(object sender, RoutedEventArgs e)
         {
@@ -85,18 +91,72 @@ namespace AdaptiveGridApp
         {
             PhotoItems.Clear();
             CurrentIndex = 0;
-            Columns = 1;
+            TotalColumns = 1;
+            TotalRows = 1;
         }
 
-        private void AspectContentControl_Loaded(object sender, RoutedEventArgs e)
+        private void ComputeAndSetMargins()
         {
+            int LastRowTotalItems = PhotoItems.Count - ((TotalRows - 1) * TotalColumns);
+            int LastRowStartIndex;
+            if (TotalRows <= 1)
+            {
+                LastRowStartIndex = 0;
+            }
+            else
+            {
+                LastRowStartIndex = (TotalRows - 1) * TotalColumns;
+            }
+            for (int i = 0; i < LastRowStartIndex; i++)
+            {
+                DependencyObject containerAtIndex = AdaptiveGridViewControl.ContainerFromIndex(i);
+                if (containerAtIndex is GridViewItem gvItem)
+                {
+                    gvItem.Margin = new Thickness(0, 0, 10, 0);
+                }
+            }
+            int CenterItemIndex = TotalColumns / 2 + TotalColumns % 2;
+            for (int i = 1; i <= LastRowTotalItems; i++)
+            {
+                int indexFactor = LastRowStartIndex - 1;
+                int index = indexFactor + i;
+                DependencyObject containerAtIndex = AdaptiveGridViewControl.ContainerFromIndex(index);
+                if (containerAtIndex is GridViewItem gvItem)
+                {
+                    if (LastRowTotalItems > CenterItemIndex)
+                    {
+                        gvItem.Margin = new Thickness(0, 0, 10, 0);
+                    }
+                    else
+                    {
+                        int RightMargin = 10;
+                        int centerX = (int)AdaptiveGridViewControl.ActualWidth / 2;
+                        int LeftMargin = centerX - ((int)((CenterItemIndex - i) * AdaptiveGridViewControl.DesiredWidth + RightMargin)) - ((int)AdaptiveGridViewControl.DesiredWidth / 2);//- ((int)AdaptiveGridViewControl.DesiredWidth / 2)
+                        if (LeftMargin < 0)
+                            LeftMargin = 0;
+                        gvItem.Margin = new Thickness(LeftMargin, 0, RightMargin, 0);
+                        DependencyObject contai = AdaptiveGridViewControl.ContainerFromItem(gvItem);
 
+                    }
+                }
+            }
+        }
+
+        private bool IsItemInLastRow(PhotoItem photoItem)
+        {
+            bool IsItemInLastRow = false;
+            int CurrentRow = (int)Math.Ceiling((PhotoItems.IndexOf(photoItem) + 1) / (double)TotalColumns);
+
+            if (CurrentRow == TotalRows)
+            {
+                IsItemInLastRow = true;
+            }
+            return IsItemInLastRow;
         }
 
         private void AdaptiveGridViewControl_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             ComputeAndSetDimension();
-            // AdaptiveGridViewControl.DesiredWidth = e.NewSize.Width / Columns;
         }
     }
 }
